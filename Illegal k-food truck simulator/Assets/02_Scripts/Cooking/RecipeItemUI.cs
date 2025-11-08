d2861fb806ee6767fd67c43f75d00483b682e86a
techn-0 <gnlrjswjd@naver.com>
2025-11-08 18:34:26 +0900

    구버전 코드 주석처리

--- Illegal k-food truck simulator/Assets/02_Scripts/Cooking/RecipeItemUI.cs
+++ Illegal k-food truck simulator/Assets/02_Scripts/Cooking/RecipeItemUI.cs
@@ -11,28 +11,28 @@ public class RecipeItemUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI cookingTimeText;
     [SerializeField] private Transform ingredientsContainer;
     [SerializeField] private GameObject ingredientSlotPrefab;
-    [SerializeField] private Button cookButton;
+    // [SerializeField] private Button cookButton; // 구버전: 이제 손님 주문 UI에서 수락 버튼으로 요리 시작
     
     private RecipeDefinition recipe;
-    private CookingManager cookingManager;
-    private bool isInitialized = false;
+    // private CookingManager cookingManager; // 구버전: 쿡 버튼 관련 기능
+    // private bool isInitialized = false; // 구버전: 쿡 버튼 상태 업데이트용
     
-    void Awake()
-    {
-        cookingManager = CookingManager.Instance;
-    }
+    // void Awake()
+    // {
+    //     cookingManager = CookingManager.Instance;
+    // }
     
-    void Start()
-    {
-        cookButton.onClick.AddListener(OnCookButtonClicked);
-        isInitialized = true;
-        
-        // Start에서 초기화가 완료된 후 버튼 상태 업데이트
-        if (recipe != null)
-        {
-            UpdateCookButton();
-        }
-    }
+    // void Start()
+    // {
+    //     cookButton.onClick.AddListener(OnCookButtonClicked);
+    //     isInitialized = true;
+    //     
+    //     // Start에서 초기화가 완료된 후 버튼 상태 업데이트
+    //     if (recipe != null)
+    //     {
+    //         UpdateCookButton();
+    //     }
+    // }
     
     public void SetupRecipe(RecipeDefinition recipeDefinition)
     {
@@ -47,21 +47,21 @@ public class RecipeItemUI : MonoBehaviour
         // 재료 목록 생성
         CreateIngredientSlots();
         
-        // Start가 호출된 후에만 버튼 상태 업데이트
-        if (isInitialized)
-        {
-            UpdateCookButton();
-        }
+        // // 구버전: Start가 호출된 후에만 버튼 상태 업데이트
+        // if (isInitialized)
+        // {
+        //     UpdateCookButton();
+        // }
     }
     
-    void Update()
-    {
-        // 초기화가 완료된 후에만 매 프레임마다 요리 가능 여부 확인
-        if (isInitialized)
-        {
-            UpdateCookButton();
-        }
-    }
+    // void Update()
+    // {
+    //     // 구버전: 초기화가 완료된 후에만 매 프레임마다 요리 가능 여부 확인
+    //     if (isInitialized)
+    //     {
+    //         UpdateCookButton();
+    //     }
+    // }
     
     private void CreateIngredientSlots()
     {
@@ -73,19 +73,21 @@ public class RecipeItemUI : MonoBehaviour
         }
     }
     
-    private void UpdateCookButton()
-    {
-        bool canCook = cookingManager.CanCookRecipe(recipe);
-        cookButton.interactable = canCook;
-        
-        // 버튼 색상 변경 (선택사항)
-        var colors = cookButton.colors;
-        colors.normalColor = canCook ? Color.white : Color.gray;
-        cookButton.colors = colors;
-    }
+    // // 구버전: 쿡 버튼 활성화/비활성화 기능 (이제 손님 주문 UI에서 처리)
+    // private void UpdateCookButton()
+    // {
+    //     bool canCook = cookingManager.CanCookRecipe(recipe);
+    //     cookButton.interactable = canCook;
+    //     
+    //     // 버튼 색상 변경 (선택사항)
+    //     var colors = cookButton.colors;
+    //     colors.normalColor = canCook ? Color.white : Color.gray;
+    //     cookButton.colors = colors;
+    // }
     
-    private void OnCookButtonClicked()
-    {
-        cookingManager.StartCooking(recipe);
-    }
+    // // 구버전: 직접 요리 시작 (이제 손님 주문 수락 -> 미니게임으로 진행)
+    // private void OnCookButtonClicked()
+    // {
+    //     cookingManager.StartCooking(recipe);
+    // }
 }
